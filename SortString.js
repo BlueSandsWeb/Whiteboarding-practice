87cba795daabd2e4f2fc59678cf4bbdfbcfc3f89
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-24 23:26:19 -0700

    wrote tests and description

---
+++ SortString.js
@@ -0,0 +1,6 @@
+// Write a function called sortString that takes a string of letters and returns a string with the letters sorted in alphabetical order.
+
+// Tests:
+console.log(sortString('dcba'))
+console.log(sortString('zycxbwa'))
+console.log(sortString('AzycxbCwBaA'))
