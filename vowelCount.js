41488a7973778449866741d7c3623e8288981e93
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-24 23:31:12 -0700

    fixed tests

--- vowelCount.js
+++ vowelCount.js
@@ -8,7 +8,6 @@ function vowelCount(str) {
     const vowels= {"a": 1,"e": 1,"i": 1,"o": 1,"u": 1}; // change to obj
     [...str].forEach((el) => {
       if(el.toLowerCase() in vowels){ // check using obj lookup O(1) time
-        console.log('RUNNING')
         count++
       }
     })
@@ -16,9 +15,9 @@ function vowelCount(str) {
   }
 
 // Tests
-// console.log(vowelCount('-bcd-fgh-jklmn-pqrst-vwxyz')); // <--- 0
-// console.log(vowelCount('Hello world!')); // <--- 3
-// console.log(vowelCount('Pinto beans')); // <--- 4
-// console.log(vowelCount('The quick brown fox jumped over the lazy dog.')); // <--- 12
-// console.log(vowelCount('Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.')); // <--- 58
-// console.log(vowelCount('All I have ever wanted is to be an Uber driver!')); // <--- 16
+console.log("should = 0: ", vowelCount('-bcd-fgh-jklmn-pqrst-vwxyz')); // <--- 0
+console.log("should = 3: ", vowelCount('Hello world!')); // <--- 3
+console.log("should = 4: ", vowelCount('Pinto beans')); // <--- 4
+console.log("should = 12: ", vowelCount('The quick brown fox jumped over the lazy dog.')); // <--- 12
+console.log("should = 58: ", vowelCount('Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.')); // <--- 58
+console.log("should = 16: ", vowelCount('All I have ever wanted is to be an Uber driver!')); // <--- 16
