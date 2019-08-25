cad579d38ec19621788303e9126944be100019c2
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-24 23:30:29 -0700

    answered

--- SortString.js
+++ SortString.js
@@ -1,6 +1,14 @@
 // Write a function called sortString that takes a string of letters and returns a string with the letters sorted in alphabetical order.
 
+function sortString(str) {
+    let arr = [];
+    for (var i = 0; i < str.length; i++) {
+      arr.push(str[i]);
+    }
+    return arr.sort().join("")
+}
+
 // Tests:
-console.log(sortString('dcba'))
-console.log(sortString('zycxbwa'))
-console.log(sortString('AzycxbCwBaA'))
+console.log(sortString('dcba'))         // =>  'abcd'
+console.log(sortString('zycxbwa'))      // =>  'abcwxyz'
+console.log(sortString('AzycxbCwBaA'))  // =>  'AABCabcwxyz'
