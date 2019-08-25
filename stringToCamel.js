b628ad3f56e2523a6e5933aec0fa5ffcd223aa61
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-24 23:33:12 -0700

    added tests and problem description

---
+++ stringToCamel.js
@@ -0,0 +1,8 @@
+// Complete the function so that it converts dash-delimited ("kebab" case) & underscore-delimited ("snake" case) words into "camel" casing. 
+// The first word within the output should be capitalized only if the original word was capitalized.
+
+// tests:
+toCamelCase("the-stealth-warrior")
+// Should return "theStealthWarrior"
+toCamelCase("The_stealth_warrior")
+// Should return "TheStealthWarrior"
