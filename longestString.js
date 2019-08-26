07b23bc7008097bb3f41601732c2f691b2a13fd5
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-25 19:43:16 -0700

    added description and tests

---
+++ longestString.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of strings and return the longest string in the array.
+
+// Tests:
+
+const strings1 = ['short', 'really, really long!', 'medium'];
+console.log(longestString(strings1)); // <--- 'really, really long!'
+
+const strings2 = ['short', 'first long string!!', 'medium', 'abcdefghijklmnopqr'];
+console.log(longestString(strings2)); // <--- 'first long string!'
