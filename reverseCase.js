48664096aa12367884c14f38f1b03925c69a274f
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-28 07:29:57 -0700

    added problem description and test cases

---
+++ reverseCase.js
@@ -0,0 +1,7 @@
+// Write a function that takes in a string, reverses the 'casing' of that string, and returns the "reversed-casing" string.
+
+let string = "HELLO world!";
+console.log(reverseCase(string)); // <--- hello WORLD!
+
+string = "HeLlO wOrLd!";
+console.log(reverseCase(string)); // <--- hElLo WoRlD
