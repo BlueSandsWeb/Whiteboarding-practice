acfdc452d66052648f56840fd3f2295d2dfc3125
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-28 07:30:25 -0700

    completed a solution to the problem

--- reverseCase.js
+++ reverseCase.js
@@ -1,5 +1,18 @@
 // Write a function that takes in a string, reverses the 'casing' of that string, and returns the "reversed-casing" string.
 
+function reverseCase(str) {
+  let arr = str.split("");
+  let arrRev = [];
+  arr.forEach(function(i) {
+    if (i === i.toUpperCase()) {
+      arrRev.push(i.toLowerCase());
+    } else {
+      arrRev.push(i.toUpperCase());
+    }
+  });
+  return arrRev.join("");
+}
+
 let string = "HELLO world!";
 console.log(reverseCase(string)); // <--- hello WORLD!
 
