b1f2d3722bbca5760fa8264b562155628a815134
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-09-07 08:57:59 -0700

    added question and tests

---
+++ commonElements.js
@@ -0,0 +1,8 @@
+// Write a function called commonElements that has parameters for two arrays.
+// Return an array of all items that are present in both arrays.
+// Do not modify the arrays that are passed in.
+
+//Tests
+// console.log(commonElements([1, 2, 3, 4], [3, 4, 5, 6])); => [3, 4]
+// console.log(commonElements(['a', 'b', 'c'] ['x', 'y', 'z', 'a'])); => ['a']
+// console.log(commonElements([1, 2, 3] [4, 5, 6])); => []
