54074ef00501b4c5c1341738743cdc3edeea5cf4
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-09-24 09:36:58 -0400

    added question

---
+++ diagonalDifference.js
@@ -0,0 +1,19 @@
+// Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+// example:
+// 11, 2, 4
+//  4, 5, 6
+// 10, 8, -12
+
+// diagonal top left -> bottom right sum = 4
+
+// diagonal top right -> bottom left sum = 19
+
+// absolute diagonal difference(subtract) = |4 - 19| 
+
+function diagonalDifference(arr) {
+  return
+}
+
+// Test1
+console.log(diagonalDifference([[11, 2, 4,], [4, 5, 6], [10, 8, -12]]))
