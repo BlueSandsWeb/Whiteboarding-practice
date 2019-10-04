8566a0f9e34aa46bbe98e9ba7c3c688f98ed533c
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-10-04 10:55:57 -0700

    completed challenge

--- hackerRank/diagonalDifference.js
+++ hackerRank/diagonalDifference.js
@@ -9,11 +9,22 @@
 
 // diagonal top right -> bottom left sum = 19
 
-// absolute diagonal difference(subtract) = |4 - 19| 
+// absolute diagonal difference(subtract) = |4 - 19|
 
 function diagonalDifference(arr) {
-  return
+  let ltr = 0;
+  let rtl = arr.length - 1;
+  let ltrSum = 0;
+  let rtlSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    ltrSum += arr[i][ltr];
+    rtlSum += arr[i][rtl];
+    ltr++;
+    rtl--;
+  }
+  return Math.abs(ltrSum - rtlSum);
 }
 
 // Test1
-console.log(diagonalDifference([[11, 2, 4,], [4, 5, 6], [10, 8, -12]]))
+console.log(15 === diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]));
