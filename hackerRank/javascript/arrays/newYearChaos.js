9901c7e703ea29d60a4613236202fe63a933e8f0
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-10-22 12:12:27 -0700

    added question

---
+++ hackerRank/javascript/arrays/newYearChaos.js
@@ -0,0 +1,33 @@
+// It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of people queued up,
+// and each person wears a sticker indicating their initial position in the queue. Initial positions increment by from at
+// the front of the line to n at the back.
+
+// Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions,
+// they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if
+// n = 8 and person 5 bribes person 4, the queue will look like this: [1,2,3,5,4,6,7,8].
+
+// Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state!
+
+// Constraints
+// 1 <= t <= 10
+// 1 <= n <= 10^5
+
+// Output Format
+
+// Print an integer denoting the minimum number of bribes needed to get the queue into its final state.
+// Print Too chaotic if the state is invalid, i.e. it requires a person to have bribed more than 2 people.
+
+// Sample Input
+
+// 2
+// 5
+// 2 1 5 3 4
+// 5
+// 2 5 1 3 4
+
+// Sample Output
+
+// 3
+// Too chaotic
+
+function minimumBribes(q) {}
