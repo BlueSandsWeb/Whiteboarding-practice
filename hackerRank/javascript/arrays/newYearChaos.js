7c2db1ff5bc66bd9448faffbe88a0a313c56287d
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-10-22 23:05:32 -0700

    initial solution tried and second solution written in pseudo code

--- hackerRank/javascript/arrays/newYearChaos.js
+++ hackerRank/javascript/arrays/newYearChaos.js
@@ -30,4 +30,32 @@
 // 3
 // Too chaotic
 
-function minimumBribes(q) {}
+function minimumBribes(q) {
+  // Second try, Do something similar to a bubble sort, that moves an array using swaps to match
+  console.log(q);
+
+  for (let i = 1; i <= q.length; i++) {}
+
+  //   First whack at it.  Doesn't account for every case
+  //   let minBribes = 0;
+  //   for (let i = 0; i < q.length - 1; i++) {
+  //     // check each item against the next item for difference.
+  //     if (q[i] > q[i + 1]) {
+  //       minBribes += q[i] - q[i + 1];
+  //     }
+  //     if (q[i] > q[i + 1] + 2) {
+  //       console.log(`Too chaotic`);
+  //       return `Too chaotic`;
+  //     }
+  //   }
+  //   console.log(minBribes);
+  //   return minBribes;
+}
+
+minimumBribes([2, 1, 5, 3, 4]); // -> 3
+minimumBribes([2, 5, 1, 3, 4]); // -> 'Too chaotic'
+minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]); // -> `Too chaotic`
+minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]); // -> 7
+minimumBribes([1, 2, 5, 3, 4, 7, 8, 6]); // -> 4
+
+// [ 1 2 5 3 7 8 6 4 ]
