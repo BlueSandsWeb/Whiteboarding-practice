c17e4bc3892343771c3b125bbeb8cba1d1557673
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-10-28 13:50:31 -0700

    answered

--- hackerRank/python/binary_search_tree_lowest_common_ancestor.py
+++ hackerRank/python/binary_search_tree_lowest_common_ancestor.py
@@ -38,33 +38,50 @@ class BinarySearchTree:
                 else:
                     break
 
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-'''
-class Node:
-      def __init__(self,info): 
-          self.info = info  
-          self.left = None  
-          self.right = None 
-           
-
-       // this is a node of the tree , which contains info as data, left , right
-'''
+# This solution assumes v1 and v2 are in the binary tree.  If there is a chance they are not, then this should be drastically different
 
 
 def lca(root, v1, v2):
-  # Enter your code here
+    lcaNode = root
+    same = True
+    while same:
+        if(lcaNode.info == v1 or lcaNode.info == v2):
+            same = False
+            return lcaNode
+        elif(lcaNode.info > v1 and lcaNode.info > v2):
+            lcaNode = lcaNode.left
+        elif(lcaNode.info < v1 and lcaNode.info < v2):
+            lcaNode = lcaNode.right
+        else:
+            same = False
+
+    return lcaNode
 
 
+# Test 1
 tree = BinarySearchTree()
-t = int(input())
-
-arr = list(map(int, input().split()))
-
+arr = [4, 2, 3, 1, 7, 6]
+t = 6
 for i in range(t):
     tree.create(arr[i])
+ans = lca(tree.root, 1, 7)
+ans = tree.root
+print(f"{ans.info} is {ans.info == 4}")
 
-v = list(map(int, input().split()))
+# # Test 2
+# tree = BinarySearchTree()
+# arr = [1, 2]
+# t = 2
+# for i in range(t):
+#     tree.create(arr[i])
+# ans = lca(tree.root, 1, 2)
+# print(f"{ans.info} is {ans.info == 1}")
 
-ans = lca(tree.root, v[0], v[1])
-print(ans.info)
+# # Test 3
+# tree = BinarySearchTree()
+# arr = [5, 3, 8, 2, 4, 6, 7]
+# t = 7
+# for i in range(t):
+#     tree.create(arr[i])
+# ans = lca(tree.root, 7, 3)
+# print(f"{ans.info} is {ans.info == 5}")
